0638df49f010817d7bf09a442225fbec42ac3b73
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-10 12:10:50 -0600

    testing adding a ui library

---
+++ src/react-ui/Modal/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { createPortal } from "react-dom";
+
+type ModalProps = {
+  open: boolean;
+  children: React.ReactNode
+}
+
+const Modal: FC<ModalProps> = ({ open, children }) => {
+  if (!open) return null;
+  return createPortal(children, document.body);
+}
+
+export default Modal;
