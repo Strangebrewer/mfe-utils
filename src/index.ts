9923d908f454212e66e88e15bfdd69b6a8e5e224
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-10 18:43:42 -0600

    fiddling...

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+import "./react-ui/theme.css";
 export * from './jwt-auth';
 export * from './react-ui';
--- src/react-ui/Button/index.tsx
+++ src/react-ui/Button/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import "../index.css";
+import "../theme.css";
 import "./styles.css";
 
 type ButtonProps = {
