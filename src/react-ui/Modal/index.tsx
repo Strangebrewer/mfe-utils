9a60c3d6cc96dd083065144be2fa5735d547b949
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-10 19:46:58 -0600

    fiddling...

--- src/react-ui/Modal/index.tsx
+++ src/react-ui/Modal/index.tsx
@@ -1,14 +1,31 @@
-import { FC } from "react";
+import React, { FC } from "react";
 import { createPortal } from "react-dom";
 
 type ModalProps = {
-  open: boolean;
+  isOpen: boolean;
+  close: () => void;
   children: React.ReactNode
 }
 
-const Modal: FC<ModalProps> = ({ open, children }) => {
-  if (!open) return null;
-  return createPortal(children, document.body);
+const Modal: FC<ModalProps> = ({ isOpen, close, children }) => {
+  if (!isOpen) return null;
+
+  function onClickOutside(e: React.BaseSyntheticEvent) {
+    if (e.target.className.includes('bka-modal-wrapper')) {
+      close();
+    }
+  }
+
+  const modalWrapper = () => {
+    return (
+      <div className="bka-modal-fade-in bka-modal-wrapper" onClick={onClickOutside}>
+        <div className="bka-modal-fade-in bka-modal-body">'
+          {children}
+        </div>
+      </div>
+    )
+  }
+  return createPortal(modalWrapper(), document.body);
 }
 
 export default Modal;
