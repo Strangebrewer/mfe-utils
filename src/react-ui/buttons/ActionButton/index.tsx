7f436ca75bf02ba5786eb504bcce9982b557c875
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-11 19:48:08 -0600

    fiddling...

--- src/react-ui/buttons/ActionButton/index.tsx
+++ src/react-ui/buttons/ActionButton/index.tsx
@@ -3,12 +3,13 @@ import "./styles.css";
 
 type ActionButtonProps = {
   iconClass: string;
-  color: 'blue' | 'red' | 'green';
+  onClick: () => any;
+  color?: 'blue' | 'red' | 'green';
 }
 
-const ActionButton: FC<ActionButtonProps> = ({ iconClass, color = 'green', ...rest }) => {
+const ActionButton: FC<ActionButtonProps> = ({ iconClass, color = 'green', onClick }) => {
   return (
-    <button className={`bka-action-btn bka-action-btn--${color}`} {...rest}>
+    <button className={`bka-action-btn bka-action-btn--${color}`} onClick={onClick}>
       <i className={iconClass} />
     </button>
   )
