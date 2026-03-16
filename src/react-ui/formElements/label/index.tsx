86d0d7075ba44a879dc171310a4df3735cbe7ac9
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-15 19:59:30 -0600

    fiddling with styles

--- src/react-ui/formElements/label/index.tsx
+++ src/react-ui/formElements/label/index.tsx
@@ -6,7 +6,7 @@ type LabelProps = {
 
 const Label: FC<LabelProps> = ({ text }) => {
   return (
-    <label className='tw:inline-block tw:text-[.9rem] tw:mb-[4px] tw:w-[100%]'>{text}</label>
+    <label className='tw:block tw:text-[.9rem] tw:mb-[4px] tw:w-[100%]'>{text}</label>
   );
 };
 
