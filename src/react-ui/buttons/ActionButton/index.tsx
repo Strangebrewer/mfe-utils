9ac3aee30c584270221fbd8c1c1f88b67401adf7
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-20 21:16:45 -0600

    add button options

--- src/react-ui/buttons/ActionButton/index.tsx
+++ src/react-ui/buttons/ActionButton/index.tsx
@@ -2,27 +2,34 @@ import React, { FC } from "react";
 import "./styles.css";
 
 type ActionButtonProps = {
-  iconClass: string;
+  iconClass?: string;
+  text?: string;
   onClick: () => any;
   color?: 'blue' | 'red' | 'green';
   size?: 'sm' | 'md' | 'lg' | 'xl';
 }
 
-const ActionButton: FC<ActionButtonProps> = ({ iconClass, color = 'green', size = 'md', onClick }) => {
+const ActionButton: FC<ActionButtonProps> = ({
+  iconClass,
+  color = 'green',
+  size = 'md',
+  text,
+  onClick
+}) => {
   const style: Obj = { fontSize: '12px' };
-  if (size !== 'md')   {
+  if (size !== 'md') {
     if (size === 'sm') style.fontSize = '9px';
     if (size === 'lg') style.fontSize = '15px';
     if (size === 'xl') style.fontSize = '18px';
   }
-  
+
   return (
     <button
       style={style}
       className={`bka-action-btn bka-action-btn--${color}`}
       onClick={onClick}
     >
-      <i className={iconClass} />
+      {iconClass ? <i className={iconClass} /> : text}
     </button>
   )
 }
