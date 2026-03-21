581f20df2e798d66b8a02cd1af54b9443911b51b
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-20 21:04:06 -0600

    add button sizing

--- src/react-ui/buttons/ActionButton/index.tsx
+++ src/react-ui/buttons/ActionButton/index.tsx
@@ -5,11 +5,23 @@ type ActionButtonProps = {
   iconClass: string;
   onClick: () => any;
   color?: 'blue' | 'red' | 'green';
+  size?: 'sm' | 'md' | 'lg' | 'xl';
 }
 
-const ActionButton: FC<ActionButtonProps> = ({ iconClass, color = 'green', onClick }) => {
+const ActionButton: FC<ActionButtonProps> = ({ iconClass, color = 'green', size = 'md', onClick }) => {
+  const style: Obj = { fontSize: '12px' };
+  if (size !== 'md')   {
+    if (size === 'sm') style.fontSize = '9px';
+    if (size === 'lg') style.fontSize = '15px';
+    if (size === 'xl') style.fontSize = '18px';
+  }
+  
   return (
-    <button className={`bka-action-btn bka-action-btn--${color}`} onClick={onClick}>
+    <button
+      style={style}
+      className={`bka-action-btn bka-action-btn--${color}`}
+      onClick={onClick}
+    >
       <i className={iconClass} />
     </button>
   )
