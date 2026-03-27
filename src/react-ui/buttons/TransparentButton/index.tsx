d537e6399009c360b58a964e9440956574627248
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-26 21:05:08 -0600

    add Transparent Button

--- src/react-ui/buttons/TransparentButton/index.tsx
+++ src/react-ui/buttons/TransparentButton/index.tsx
@@ -1,11 +1,36 @@
 import React, { FC } from "react";
 import "./styles.css";
 
-type TransparentButtonProps = {};
+type TransparentButtonProps = {
+  onClick: () => any;
+  text: string;
+  color?: 'blue' | 'red' | 'indigo' | 'green';
+  size?: 'sm' | 'md' | 'lg' | 'xl';
+  title?: string;
+};
 
-const TransparentButton: FC<TransparentButtonProps> = () => {
+const TransparentButton: FC<TransparentButtonProps> = ({
+  onClick,
+  text,
+  color = 'blue',
+  size = 'md',
+  title,
+}) => {
+  const style: Obj = { fontSize: '16px' };
+  if (size !== 'md') {
+    if (size === 'sm') style.fontSize = '12px';
+    if (size === 'lg') style.fontSize = '20px';
+    if (size === 'xl') style.fontSize = '24px';
+  }
   return (
-    <button></button>
+    <button
+      style={style}
+      title={title}
+      className={`bka-transparent-btn bka-transparent-btn-${color}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 };
 
