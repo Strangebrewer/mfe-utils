202c82e94c97e48b2188ecfacd5830af1c2b5841
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-03-26 20:27:00 -0600

    add Text Button

---
+++ src/react-ui/buttons/TransparentButton/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import "./styles.css";
+
+type TransparentButtonProps = {};
+
+const TransparentButton: FC<TransparentButtonProps> = () => {
+  return (
+    <button></button>
+  );
+};
+
+export default TransparentButton;
